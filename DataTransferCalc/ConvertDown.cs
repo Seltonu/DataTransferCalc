a7dc1c2c8b79ec353d0174f78854abc3e9740898
Spooks <Spooks@SPOOKYBOX>
2016-06-12 00:57:07 -0700

    Add project files.

---
+++ DataTransferCalc/ConvertDown.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+public class ConvertDown
+{
+    public string findType(string input)
+    {
+        string type = "";
+
+        if (input.Contains("Kb"))
+        {
+            type = "Kb";
+        }
+        if (input.Contains("KB") || input.Contains("kb"))
+        {
+            type = "KB";
+        }
+        if (input.Contains("Mb"))
+        {
+            type = "Mb";
+        }
+        if (input.Contains("MB") || input.Contains("mb"))
+        {
+            type = "MB";
+        }
+        if (input.Contains("Gb"))
+        {
+            type = "Gb";
+        }
+        if (input.Contains("GB") || input.Contains("gb"))
+        {
+            type = "GB";
+        }
+        if (input.Contains("Tb"))
+        {
+            type = "Tb";
+        }
+        if (input.Contains("TB") || input.Contains("tb"))
+        {
+            type = "TB";
+        }
+
+        return type;
+    }
+
+    public double ConvertToBytes(string numbersAsString, string type)
+    {
+
+        double value = 0; //placeholder value
+
+        if (numbersAsString.Length > 0) //checks if it found numbers, otherwise try again
+            value = Convert.ToDouble(numbersAsString);
+        else
+        {
+            Console.WriteLine("Try again please, enter a valid value.");
+        }
+
+
+        switch (type)
+        {
+            case "":
+                Console.WriteLine("Switch 'Type' received a null input.");
+                Console.ReadLine();
+                break;
+            case "Kb":
+                return (value * 125);
+            case "KB":
+                return (value * 1000);
+            case "Mb":
+                return (value * 125000);
+            case "MB":
+                return (value * 1e+6);
+            case "Gb":
+                return (value * 1.25e+8);
+            case "GB":
+                return (value * 1e+9);
+            case "Tb":
+                return (value * 1.25e+11);
+            case "TB":
+                return (value * 1e+12);
+            default:
+                Console.WriteLine("Try again.");
+                Console.ReadLine();
+                break;
+        }
+
+        return 0; //placeholder, should never need this
+    }
+
+    public string retrieveNumbers(string input)
+    {
+        string numbersAsString = "";
+
+        for (int i = 0; i < input.Length; i++) // goes through each char of the input string searching for digits, concatenates into a string
+        {
+            if (Char.IsDigit(input[i]))
+                numbersAsString += input[i];
+            if (input[i] == '.')
+                numbersAsString += input[i];
+        }
+
+        return numbersAsString;
+    }
+    
+}
---
+++ DataTransferCalc/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertDown cdown = new ConvertDown();
+
+            Console.WriteLine("\t\t\t\t\\\\\\\\\\ Data-Transfer Time Calculator v1.0 /////");
+            Console.WriteLine("Type help for help.\n");
+            Console.Write("Amount of Data: ");
+            string input = Console.ReadLine();
+            string numbersAsString = cdown.retrieveNumbers(input);
+            string typeOfFile = cdown.findType(input);
+
+            if ((input.ToLower()) == "help")
+            {
+                Console.WriteLine("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+                Console.WriteLine("Utilizing the Data-Transfer program is easy. Just type in a number and corresponding file type when prompted.");
+                Console.WriteLine("\nSupported File types are: \nKb \tKB \nMb \tMB \nGb \tGB \nTb \tTB\n");
+                Console.WriteLine("If your results seem off, you may be using the wrong capitalization in your file type.");
+                Console.WriteLine("Lowercase 'b' is bits, uppercase 'B' is bytes.");
+                Console.WriteLine("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+            }   
+
+            while (numbersAsString == "" || typeOfFile == "")
+            {
+                Console.WriteLine("Please enter a valid file size and type.");
+                input = Console.ReadLine();
+                numbersAsString = cdown.retrieveNumbers(input);
+                typeOfFile = cdown.findType(input);
+            }
+            double fileSizeInBytes = cdown.ConvertToBytes(numbersAsString, typeOfFile);
+
+
+
+
+            Console.Write("Transfer Speed (per second): ");
+            input = Console.ReadLine();
+
+            numbersAsString = cdown.retrieveNumbers(input); //these variables were declared above. We're re-using them to avoid creating new ones.
+            typeOfFile = cdown.findType(input);
+
+            if ((input.ToLower()) == "help")
+            {
+                Console.WriteLine("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+                Console.WriteLine("Utilizing the Data-Transfer program is easy. Just type in a number and corresponding file type when prompted.");
+                Console.WriteLine("Supported File types are: Kb, KB, Mb, MB, Gb, GB, Tb, TB");
+                Console.WriteLine("If your results seem off, you may be using the wrong capitalization in your file type.");
+                Console.WriteLine("Lowercase 'b' is bits, uppercase 'B' is bytes. Both lower, we assume bytes.");
+                Console.WriteLine("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+            }
+
+            while (numbersAsString == "" || typeOfFile == "")
+            {
+                Console.WriteLine("Please enter a valid file size and type.");
+                input = Console.ReadLine();
+                numbersAsString = cdown.retrieveNumbers(input);
+                typeOfFile = cdown.findType(input);
+            }
+            double transferSpeedInBytes = cdown.ConvertToBytes(numbersAsString, typeOfFile);
+
+
+            
+
+            int timeInSeconds = Convert.ToInt32(fileSizeInBytes / transferSpeedInBytes);
+
+            if (timeInSeconds > 0)
+            {
+                int hours = 0;
+                int minutes = timeInSeconds / 60;
+                int seconds = timeInSeconds % 60;
+
+                if (minutes >= 60)
+                {
+                    hours = minutes / 60;
+                    minutes = minutes % 60;
+                }
+
+                if (hours >= 1)
+                {
+                    Console.WriteLine("\n.....Will take " + Convert.ToString(hours) + " hours " + Convert.ToString(minutes) + " minutes and " + Convert.ToString(seconds) + " seconds.");
+                }
+                else if (minutes >= 1)
+                {
+                    Console.WriteLine("\n.....Will take " + Convert.ToString(minutes) + " minutes and " + Convert.ToString(seconds) + " seconds.");
+                }
+                else
+                {
+                    Console.WriteLine("\n.....Will take " + Convert.ToString(seconds) + " seconds.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n.....It would take less than a second.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
